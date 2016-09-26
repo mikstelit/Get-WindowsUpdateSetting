e195cd2bbab1f8470a4e61a08f3918a9579de023
Mike.Littlefield@live.com <mike.littlefield@live.com>
2016-09-26 12:18:18 -0700

    Initial commit

---
+++ Get-WindowsUpdateSetting.ps1
@@ -0,0 +1,69 @@
+﻿<#
+.Synopsis
+   Outputs the Windows Update setting on a computer.
+.DESCRIPTION
+   Takes a string array of computer names and outputs 
+   to the screen the computer's Windows Update setting.
+.EXAMPLE
+   Get-WindowsUpdateSetting $ComputerNames
+.EXAMPLE
+   $ComputerNames | Get-WindowsUpdateSetting
+#>
+Function Get-WindowsUpdateSetting
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string[]]$ComputerNames
+    )
+
+    Process
+    {
+        Foreach ($ComputerName in $ComputerNames)
+        {
+            Try
+            {
+                $Registry = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $ComputerName)
+                $RegistryKey= $Registry.OpenSubKey('SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Auto Update')
+                $AUOptions = $RegistryKey.GetValue('AUOptions')
+
+                If ($AUOptions -eq '1')
+                {
+                    $UpdateSetting = 'Never check for updates (not recommended)'
+                }
+                ElseIf ($AUOptions -eq '2')
+                {
+                    $UpdateSetting = 'Check for updates but let me choose whether to download and install them'
+                }
+                ElseIf ($AUOptions -eq '3')
+                {
+                    $UpdateSetting = 'Download updates but let me choose whether to install them'
+                }
+                ElseIf ($AUOptions -eq '4')
+                {
+                    $UpdateSetting = 'Install updates automatically (recommended)'
+                }
+                Else
+                {
+                    $UpdateSetting = "Unknown Windows Update setting"
+                }
+
+            }
+            Catch
+            {
+                $UpdateSetting = "Unable to connect to system's registry"
+            }
+
+            [psobject]$System = @{
+                'Name' = $ComputerName;
+                'UpdateSetting' = $UpdateSetting
+                }
+
+            Write-Output ('{0}: {1}' -f $System.Name,$System.UpdateSetting)
+        }
+    }
+}
